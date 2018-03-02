fa26bff9b09a24580b9e7c8b0adc83af98e65437
wang33 <wang1386528@gmail.com>
2018-03-02 14:22:30 +0800

    拖动star

---
+++ HelloCocos2d/assets/Script/DragStar.js
@@ -0,0 +1,56 @@
+// Learn cc.Class:
+//  - [Chinese] http://www.cocos.com/docs/creator/scripting/class.html
+//  - [English] http://www.cocos2d-x.org/docs/editors_and_tools/creator-chapters/scripting/class/index.html
+// Learn Attribute:
+//  - [Chinese] http://www.cocos.com/docs/creator/scripting/reference/attributes.html
+//  - [English] http://www.cocos2d-x.org/docs/editors_and_tools/creator-chapters/scripting/reference/attributes/index.html
+// Learn life-cycle callbacks:
+//  - [Chinese] http://www.cocos.com/docs/creator/scripting/life-cycle-callbacks.html
+//  - [English] http://www.cocos2d-x.org/docs/editors_and_tools/creator-chapters/scripting/life-cycle-callbacks/index.html
+
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        // foo: {
+        //     // ATTRIBUTES:
+        //     default: null,        // The default value will be used only when the component attaching
+        //                           // to a node for the first time
+        //     type: cc.SpriteFrame, // optional, default is typeof default
+        //     serializable: true,   // optional, default is true
+        // },
+        // bar: {
+        //     get () {
+        //         return this._bar;
+        //     },
+        //     set (value) {
+        //         this._bar = value;
+        //     }
+        // },
+    },
+
+    onLoad: function () {
+        this.node.on(cc.Node.EventType.TOUCH_START, function (event) {
+            this.opacity = 100;
+        }, this.node);
+        this.node.on(cc.Node.EventType.TOUCH_MOVE, function (event) {
+            this.opacity = 100;
+            var delta = event.touch.getDelta();
+            this.x += delta.x;
+            this.y += delta.y;
+        }, this.node);
+        this.node.on(cc.Node.EventType.TOUCH_END, function (event) {
+            this.opacity = 255;
+        }, this.node)
+    },
+
+    // LIFE-CYCLE CALLBACKS:
+
+    // onLoad () {},
+
+    start () {
+
+    },
+
+    // update (dt) {},
+});
